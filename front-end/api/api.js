d7572ef51bbc25fb6053d8ee0e1df2bdb1034ea6
cesarwork <cesar@workinformatica.com.br>
2025-05-10 23:57:17 -0300

    correcoes server 2

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const { NODE_ENV } = process.env;
-const URL = "https://deploy-jornada-full-stack-2.onrender.com";
+const URL = "https://deploy-jornada-full-stack-2.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
